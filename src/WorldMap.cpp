342093c8c7df9fe203618675d65fd1dc17908b51
Ron Avital <ronavital2@gmail.com>
2024-06-02 16:09:26 +0300

    asdf

--- src/WorldMap.cpp
+++ src/WorldMap.cpp
@@ -49,5 +49,5 @@ void WorldMap::defineObj(sf::Color color, Row& row, int posX, int posY)
 		row.push_back(Block(m_resources.getPlayerTexture(5), sf::Color::Red, sf::Vector2f(posX * 60, posY * 60)));
 	}
 	//if(color== sf::Color::Green) {return }
-	//if(color== sf::Color) {return }
+	//if(color== sf::Color) {return } //
 }

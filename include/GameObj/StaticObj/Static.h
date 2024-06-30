5ac19d28314f4516f8227fa12a98ec7eb9eb4bec
david weschler <davidweschler8@gmail.com>
2024-06-30 17:30:59 +0300

    fixed static

--- include/GameObj/StaticObj/Static.h
+++ include/GameObj/StaticObj/Static.h
@@ -5,10 +5,11 @@
 class Static : public Object
 {
 public:
-	Static(std::unique_ptr<b2World>& world, sf::Color color, sf::Vector2f position, b2BodyType bodyType);
+	Static(std::unique_ptr<b2World>& world, sf::Color color, sf::Vector2f position, b2BodyType bodyType,
+		sf::Vector2f boxPosition, sf::Vector2f boxShapeSize, bool sensor);
 	virtual ~Static() = default;
 
-	void initBox(std::unique_ptr<b2World>& world, b2BodyType bodyType);	//private?
 private:
+	void initBox(std::unique_ptr<b2World>& world, b2BodyType bodyType, sf::Vector2f boxPosition, sf::Vector2f boxShapeSize, bool sensor);
 	b2Body* m_box;
 };
--- src/GameObj/StaticObj/Static.cpp
+++ src/GameObj/StaticObj/Static.cpp
@@ -1,70 +1,92 @@
 #include "GameObj/StaticObj/Static.h"
 
-Static::Static(std::unique_ptr<b2World>& world, sf::Color color, sf::Vector2f position, b2BodyType bodyType)
+Static::Static(std::unique_ptr<b2World>& world, sf::Color color, sf::Vector2f position, b2BodyType bodyType,
+	sf::Vector2f boxPosition, sf::Vector2f boxShapeSize, bool sensor)
 	: Object(color, position)
 {
-	initBox(world, bodyType);
+	initBox(world, bodyType, boxPosition, boxShapeSize, sensor);
 }
 
-void Static::initBox(std::unique_ptr<b2World>& world, b2BodyType bodyType)
+void Static::initBox(std::unique_ptr<b2World>& world, b2BodyType bodyType, 
+	sf::Vector2f boxPosition, sf::Vector2f boxShapeSize, bool sensor)
 {
 	b2FixtureDef fixtureDef;
 	b2PolygonShape boxShape;
 	b2BodyDef bodyDef;
 
 	bodyDef.type = bodyType;
-	bodyDef.position.Set(getPosition().x / 30, getPosition().y / 30);
+	bodyDef.position.Set(getPosition().x / 30, getPosition().y / 30);	// need?
 	bodyDef.userData.pointer = reinterpret_cast<uintptr_t>(this);
-	//make better
-	if (getColor() == SPACESHIP_PORTAL_C || 
-		getColor() == GRAVITY_PORTAL_C || 
-		getColor() == FORWARD_PORTAL_C ||
-		getColor() == UPSIDESPACESHIP_PORTAL_C)
-	{
-		bodyDef.position.Set(getPosition().x / 30, (120+getPosition().y) / 30);
-		boxShape.SetAsBox(0.8f, 6.0f);
-	}
-	else if (getColor() == FINISH_PORTAL_C)
-	{
-		bodyDef.position.Set(getPosition().x / 30, (60 + getPosition().y) / 30);
-		boxShape.SetAsBox(0.8f, 15.0f);
-	}
-	else if (getColor() == GROUND_JUMP_C || getColor()== GROUND_JUMP_U_C)
-	{
-		boxShape.SetAsBox(1.0f, 0.3f);
-	}
-	else if (getColor() == AIR_JUMP_C)
-	{
-		boxShape.SetAsBox(2.0f, 1.5f);
-	}
-	else if (getColor() == AIR_SPIKE_C)
-	{
-		boxShape.SetAsBox(2.5f, 2.5f);
-	}
-	else if (getColor() == LONG_SPIKE_C || getColor() == DOWN_LONG_SPIKE_C)
-	{
-		bodyDef.position.Set((4*60+getPosition().x) / 30, getPosition().y / 30);
-		boxShape.SetAsBox(8.0f, 0.4f);
-	}
-	else
-	{
-		boxShape.SetAsBox(1.0f, 1.0f);
-	}
 
-	m_box = world->CreateBody(&bodyDef);
+	bodyDef.position.Set(boxPosition.x, boxPosition.y);
 
-	if (getColor() == BLOCK_C || getColor() == BLOCK_M_C || getColor()== BLOCK_V_C)
-	{
-		fixtureDef.isSensor = false;	//default
-	}
-	else
-	{
-		fixtureDef.isSensor = true;
-	}
+	boxShape.SetAsBox(boxShapeSize.x, boxShapeSize.y);
 
+	fixtureDef.isSensor = sensor;
 	fixtureDef.shape = &boxShape;
 	fixtureDef.density = 5.0f;
 
-
 	m_box->CreateFixture(&fixtureDef);
+
+	////make better
+	//if (getColor() == SPACESHIP_PORTAL_C || 
+	//	getColor() == GRAVITY_PORTAL_C || 
+	//	getColor() == FORWARD_PORTAL_C ||
+	//	getColor() == UPSIDESPACESHIP_PORTAL_C)
+	//{
+	//	bodyDef.position.Set(getPosition().x / 30, (120+getPosition().y) / 30);
+	//	boxShape.SetAsBox(0.8f, 6.0f);
+	//}
+	//else if (getColor() == FINISH_PORTAL_C)
+	//{
+	//	bodyDef.position.Set(getPosition().x / 30, (60 + getPosition().y) / 30);
+	//	boxShape.SetAsBox(0.8f, 15.0f);
+	//}
+	// 
+	
+
+
+	//else if (getColor() == GROUND_JUMP_C || getColor()== GROUND_JUMP_U_C)
+	//{
+	//	boxShape.SetAsBox(1.0f, 0.3f);
+	//}
+	// 
+	
+	//else if (getColor() == AIR_JUMP_C)
+	//{
+	//	boxShape.SetAsBox(2.0f, 1.5f);
+	//}
+	// 
+	// 
+	
+	//else if (getColor() == AIR_SPIKE_C)
+	//{
+	//	boxShape.SetAsBox(2.5f, 2.5f);
+	//}
+	// 
+	// 
+
+
+	
+	//else if (getColor() == LONG_SPIKE_C || getColor() == DOWN_LONG_SPIKE_C)
+	//{
+	//	bodyDef.position.Set((4*60+getPosition().x) / 30, getPosition().y / 30);
+	//	boxShape.SetAsBox(8.0f, 0.4f);
+	//}
+	//else
+	//{
+	//	boxShape.SetAsBox(1.0f, 1.0f);
+	//}
+
+	//m_box = world->CreateBody(&bodyDef);
+
+	//if (getColor() == BLOCK_C || getColor() == BLOCK_M_C || getColor()== BLOCK_V_C)
+	//{
+	//	fixtureDef.isSensor = false;	//default
+	//}
+	//else
+	//{
+	//	fixtureDef.isSensor = true;
+	//}
+
 }
